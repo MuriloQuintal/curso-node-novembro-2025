dcd5d03859186b7e6f47d26eec75ab6ff8f20afd
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-11-11 22:32:52 -0300

    node

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 
 const express = require('express') //carregar a biblioteca , ou o micro framework
 const app = express()
-app.get('/', function (req, res){
-    res.send("H1>Hello World")
-})
-app.get('/oi', function (req, res){
-    res.send("Olá Mario")
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"]
+    
 
+app.get('/herois', function(req, res){
+   res.send(herois)
+})
+
+app.get('/herois/:id', function(req, res){
+    const id = req.params.id
+    res.send(herois[id-1])
+})
+app.use(express.json())
+app.post('/herois', function (req,res){
+    //let novoHeroi = req.body.chuchu
+    herois.push(req.body.chuchu)
+    herois.push(req.body.asd)
+    res.send(`ok ${req.body.chuchu}`)
 })
 
 app.listen(3000) // definir porta do servidor para sair com a api
